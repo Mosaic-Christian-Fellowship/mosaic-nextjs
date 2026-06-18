c21f2c33fd07920386f43259b5a21aa1958ebeb6
Dave Yoon <kamicrafted@pm.me>
2026-06-17 23:54:42 -0400

    feat(sanity): add homePageHeroQuery

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -7,3 +7,14 @@ export const ministriesQuery = groq`*[_type == "ministry"] | order(order asc)`
 export const serviceTimesQuery = groq`*[_type == "serviceTime"] | order(order asc)`
 export const landingPageQuery = groq`*[_type == "landingPage" && slug.current == $slug][0]`
 export const allLandingPagesQuery = groq`*[_type == "landingPage"]{ slug }`
+
+export const homePageHeroQuery = groq`*[_type == "homePage"][0]{
+  heroHeading,
+  heroSubtext,
+  heroCta1Text,
+  heroCta1Href,
+  heroCta2Text,
+  heroCta2Href,
+  heroImage,
+  "heroVideoUrl": heroVideo.asset->url
+}`
